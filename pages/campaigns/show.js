2b8d95055033401bd4d3125a49f7053aa46197a4
Jonathan Viray <jfviray7@gmail.com>
2018-11-07 22:32:05 -0500

    Add functionality to contriubute form

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Grid } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign.js';
 import web3 from '../../ethereum/web3';
+import ContributeForm from '../../components/ContributeForm';
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -11,6 +12,7 @@ class CampaignShow extends Component {
     const summary = await campaign.methods.getSummary().call();
 
     return {
+      address: props.query.address,
       minimumContribution: summary[0],
       balance: summary[1],
       requestsCount: summary[2],
@@ -38,22 +40,22 @@ class CampaignShow extends Component {
       {
         header: minimumContribution,
         meta: 'Minimum Contribution (wei)',
-        description: 'You must contribute at least this much wei to become an approver.'
+        description: 'You must contribute at least this much wei to participate.'
       },
       {
         header: requestsCount,
         meta: 'Number of Requests',
-        description: 'A request tries to withdraw money from the contract. Request must be approved by approvers.'
+        description: 'A request tries to withdraw money from the contract. Requests must be approved by contributors.'
       },
       {
         header: contributionCount,
         meta: 'Number of Contributors',
-        description: 'Number of people who have already donated to this campaign.'
+        description: 'Number of people who have contributed to this campaign.'
       },
       {
         header: web3.utils.fromWei(balance, 'ether'),
         meta: 'Campaign Balance (ether)',
-        description: 'The balance is how much money this campaign has to spend.'
+        description: 'The balance is how much money this campaign has raised.'
       },
     ];
 
@@ -64,7 +66,15 @@ class CampaignShow extends Component {
     return (
       <Layout>
         <h3>Campaign Details</h3>
-        {this.renderCards()}
+        <Grid>
+          <Grid.Column width={10}>
+            {this.renderCards()}
+          </Grid.Column>
+
+          <Grid.Column width={6}>
+            <ContributeForm address={this.props.address} />
+          </Grid.Column>
+        </Grid>
       </Layout>
     );
   }
